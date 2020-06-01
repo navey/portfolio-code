5990f2a59a726cf97e0b3e7e0929b85307598cac
navey <naveenan.r.y@gmail.com>
2020-06-01 03:38:23 -0400

    Created Home page to show my face and links to my professional accounts, removed Contact Me page

--- App.js
+++ App.js
@@ -8,9 +8,9 @@ import {
 import './App.css';
 import './naveyStyle.css';
 
-import Home from './Home';
-import Education from './Education';
-import Projects from './Projects'
+import Home from './Components/Home';
+import Education from './Components/Education';
+import Projects from './Components/Projects'
 
 export default class App extends React.Component{
   render(){
@@ -23,12 +23,11 @@ export default class App extends React.Component{
               </div>
   
               <div className="navigate">
-                <a><NavLink to="/contact">Contact Me</NavLink></a>
                 <a><NavLink to="/skills">Skills</NavLink></a>
-                <a><NavLink to="/projects">Projects</NavLink></a>
                 <a><NavLink to="/experience">Experience</NavLink></a>
+                <a><NavLink to="/projects">Projects</NavLink></a>
                 <a><NavLink to="/education">Education</NavLink></a>
-                <a><NavLink to="/">Home</NavLink></a>
+                <a><NavLink exact to="/">Home</NavLink></a>
               </div> 
             </div>
             <div>
--- Components/Home.js
+++ Components/Home.js
@@ -3,7 +3,13 @@ import React from 'react';
 import '../App.css';
 import '../naveyStyle.css';
 
-import grievous from '../Images/grievous.jpg';
+import profile from '../Images/profile.jpg';
+
+import github from '../Images/github.png'
+import resume from '../Images/CS2022_NaveenanY.pdf'
+import resume_pic from '../Images/resume.png'
+import linkedin from '../Images/linkedin.png'
+import hackerrank from '../Images/hackerrank.jpg'
 
 class Home extends React.Component{
   constructor(props) {
@@ -21,19 +27,16 @@ class Home extends React.Component{
       <div>
           <body>
             <div className="main">
-  
-              <h1 className="intro">
-                  *Suspenseful Music*
-              </h1>
-  
-              <p>
-                <button className="quote" onClick={this.response}>
-                  Hello there
-                </button>
-              </p>
-  
-              <img className="center" id="grievousPIC" src={grievous} style={{display:'none'}}/>
-              <h1 className="response" id="grievous"></h1>
+
+              <img src={profile} className='profile-pic' alt='Profile'/>
+              <h1 className='home-name'>Naveenan Yogeswaran</h1>
+              
+              <div>
+                <a href="https://github.com/navey" target="_blank" rel="noopener noreferrer"><img src={github} className='home-pic' /></a>
+                <a href={resume} target="_blank" rel="noopener noreferrer"><img src={resume_pic} className='home-pic' /></a>
+                <a href="https://www.linkedin.com/in/naveenan-yogeswaran/" target="_blank" rel="noopener noreferrer"><img src={linkedin} className='home-pic' /></a>
+                <a href="https://www.hackerrank.com/naveenan_r_y" target="_blank" rel="noopener noreferrer"><img src={hackerrank} className='home-pic' /></a>
+              </div>
   
             </div>
   
